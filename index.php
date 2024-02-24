68f35fb900f16f4b0e5791fac750e29e2efac397
kodjoSoule <souleymanekodjo@gmail.com>
2024-02-24 01:56:02 +0000

    Update jumbotron image and heading

--- index.php
+++ index.php
@@ -5,7 +5,10 @@ include 'header.php';
 <body>
     <div class="container mt-5">
         <div class="jumbotron">
-            <h1 class="display-4">Bienvenue chez Fall Automobile</h1>
+            <img src="https://th.bing.com/th/id/R.c6969a77ace0283bb61c61417048cff7?rik=LAjc%2bcwbhy2y8A&pid=ImgRaw&r=0" alt="Fall Automobile" class="img-fluid rounded" style="max-width: 100%;
+
+            ">
+            <h1 class=" display-4">Bienvenue chez Fall Automobile</h1>
             <p class="lead">Votre partenaire automobile pour l'achat, la vente et la location de voitures de luxe.</p>
             <hr class="my-4">
             <p>Explorez notre parc automobile exclusif et trouvez la voiture de vos rêves.</p>
